f680b502d71496681bef32c827742928ed1ff9a1
Erik Gafni <egafni@gmail.com>
2014-08-27 13:46:59 -0700

    add exit_status_only arg

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="psprofile",
-    version='0.1.9',
+    version='0.1.11',
     description="Collects resource usage of an executed script",
     url="http://github.com/egafni/PsProfile",
     author="Erik Gafni",
